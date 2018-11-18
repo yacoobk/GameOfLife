04d642c6cb6a95320d5222d9b93a272f2ce1a570
yacoobk <yacoobk@users.noreply.github.com>
2018-11-18 16:52:19 +0000

    first yolo implementation with 1 worker

--- LifeWorker/src/LifeWorker.cs
+++ LifeWorker/src/LifeWorker.cs
@@ -1,10 +1,10 @@
-// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
-
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using Improbable;
 using Improbable.Worker;
 
-namespace Demo
+namespace Cell
 {
     class LifeWorker
     {
@@ -12,10 +12,8 @@ namespace Demo
         private const string LoggerName = "LifeWorker.cs";
         private const int ErrorExitStatus = 1;
         private const uint GetOpListTimeoutInMilliseconds = 100;
-        private static readonly string[] hellos = {
-            "Hello", "Bonjour", "Ciao", "Guten Tag", "nĭ hăo" // A selection of Greetings in arbitrary languages
-        };
-        private static readonly Random random = new Random();
+        const int FramesPerSecond = 1;
+        const int gridSize = 10; // yolo hack for now
 
         static int Main(string[] arguments)
         {
@@ -38,17 +36,17 @@ namespace Demo
             Console.WriteLine("Worker Starting...");
             using (var connection = ConnectWorker(arguments))
             {
-                using (var dispatcher = new Dispatcher())
+                using (var view = new View())
                 {
                     var isConnected = true;
 
-                    dispatcher.OnDisconnect(op =>
+                    view.OnDisconnect(op =>
                     {
                         Console.Error.WriteLine("[disconnect] {0}", op.Reason);
                         isConnected = false;
                     });
 
-                    dispatcher.OnLogMessage(op =>
+                    view.OnLogMessage(op =>
                     {
                         connection.SendLogMessage(op.Level, LoggerName, op.Message);
                         Console.WriteLine("Log Message: {0}", op.Message);
@@ -59,25 +57,139 @@ namespace Demo
                         }
                     });
 
-                    dispatcher.OnCommandRequest<PingResponder.Commands.Ping>(request =>
-                    {
-                        connection.SendLogMessage(LogLevel.Info, LoggerName, "Received GetWorkerType command");
-
-                        var greeting = hellos[random.Next(hellos.Length)];
-                        var pingResponse = new Pong(WorkerType, String.Format("{0}, World!", greeting));
-                        var commandResponse = new PingResponder.Commands.Ping.Response(pingResponse);
-                        connection.SendCommandResponse(request.RequestId, commandResponse);
-                    });
-
                     connection.SendLogMessage(LogLevel.Info, LoggerName,
                         "Successfully connected using TCP and the Receptionist");
+                    
+                    var maxWait = System.TimeSpan.FromMilliseconds(1000f / FramesPerSecond);
+                    var stopwatch = new System.Diagnostics.Stopwatch();
+                    var isViewComplete = false;
+                    var isSeeded = false;
+
+                    // yolo - we know the gridSize, and there's only 1 worker...
+                    var cellPositions = new Improbable.EntityId[gridSize,gridSize];
+                    view.OnAddComponent<Improbable.Position>(op => 
+                    {
+                        var coords = op.Data.Get().Value.coords;
+                        // yolo - we know the coords will be integer values
+                        var x = (int)coords.x;
+                        var z = (int)coords.z;
+                        cellPositions[x,z] = op.EntityId;
+                    });
+                    var cellsAuthoritative = new HashSet<Improbable.EntityId>();
+                    view.OnAuthorityChange<Cell.CellState>(op =>
+                    {
+                        switch(op.Authority){
+                            case Improbable.Worker.Authority.Authoritative:
+                                cellsAuthoritative.Add(op.EntityId);
+                                break;
+                            case Improbable.Worker.Authority.NotAuthoritative:
+                                cellsAuthoritative.Remove(op.EntityId);
+                                break;
+                        }
+                    });
+
+                    var tick = 0;
                     while (isConnected)
                     {
+                        stopwatch.Reset();
+                        stopwatch.Start();
+                        // Invoke callbacks.
                         using (var opList = connection.GetOpList(GetOpListTimeoutInMilliseconds))
                         {
-                            dispatcher.Process(opList);
+                            view.Process(opList);
                         }
+                        // Do other work here...
+
+                        //connection.SendLogMessage(LogLevel.Info, LoggerName, "entity count"+view.Entities.Count);
+
+                        // Check if the view is complete
+                        // yolo - I know there are only gridSize*gridSize entities in the snapshot, and only 1 worker
+                        if (!isViewComplete && view.Entities.Count == gridSize*gridSize)
+                        {
+                            isViewComplete = true;
+                        }
+
+                        // Seed the game
+                        if (isViewComplete && !isSeeded)
+                        {
+                            //var update = new Cell.CellState.Update();
+                            var update = new Cell.CellState.Update();
+                            update.isAlive = true;
+                            // connection.SendComponentUpdate(new EntityId(1), update);
+                            // connection.SendComponentUpdate(new EntityId(2), update);
+                            // connection.SendComponentUpdate(new EntityId(3), update);
+                            connection.SendComponentUpdate(new EntityId(11), update);
+                            connection.SendComponentUpdate(new EntityId(12), update);
+                            connection.SendComponentUpdate(new EntityId(13), update);
+                            // connection.SendComponentUpdate(new EntityId(21), update);
+                            // connection.SendComponentUpdate(new EntityId(22), update);
+                            // connection.SendComponentUpdate(new EntityId(23), update);
+                            isSeeded = true;
+                        }
+
+                        // compute a game tick
+                        // Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent.
+                        // At each step in time, the following transitions occur:
+                        // 1. Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+                        // 2. Any live cell with two or three live neighbors lives on to the next generation.
+                        // 3. Any live cell with more than three live neighbors dies, as if by overpopulation.
+                        // 4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+                        if (isViewComplete && isSeeded)
+                        {
+                            tick++;
+                            connection.SendLogMessage(LogLevel.Info, LoggerName, $"** Tick {tick} **");
+                            foreach(var c in cellsAuthoritative)
+                            {
+                                var coords = view.Entities[c].Get<Position>().Value.Get().Value.coords;
+                                var x = (int)coords.x;
+                                var z = (int)coords.z;
+                                var isAlive = view.Entities[c].Get<CellState>().Value.Get().Value.isAlive;
+                                var neighbours = new List<Improbable.EntityId>();
+                                var liveNeighbours = 0;
+                                var maxIndex = gridSize - 1;
+
+                                // find neighbours
+                                if (x > 0) {neighbours.Add(cellPositions[x - 1, z]);} // left
+                                if (x > 0 && z < maxIndex) {neighbours.Add(cellPositions[x - 1, z + 1]);} // left, up
+                                if (x > 0 && z > 0) {neighbours.Add(cellPositions[x - 1, z - 1]);} // left, down
+                                if (z < maxIndex) {neighbours.Add(cellPositions[x , z + 1]);} // up
+                                if (z > 0) {neighbours.Add(cellPositions[x, z - 1]);} // down
+                                if (x < maxIndex) {neighbours.Add(cellPositions[x + 1, z]);} // right
+                                if (x < maxIndex && z < maxIndex) {neighbours.Add(cellPositions[x + 1, z + 1]);} // right, up
+                                if (x < maxIndex && z > 0) {neighbours.Add(cellPositions[x + 1, z - 1]);} // right, down
+
+                                // count how many are alive
+                                foreach (var n in neighbours)
+                                {
+                                    if (view.Entities[n].Get<CellState>().Value.Get().Value.isAlive) { liveNeighbours++; }
+                                }
+
+                                // if state needs to change then send an update
+                                //connection.SendLogMessage(LogLevel.Info, LoggerName, $"Cell {x},{z} {isAlive}. live neighbours: {liveNeighbours}");
+                                var update = new Cell.CellState.Update();
+                                if (isAlive && (liveNeighbours < 2 || liveNeighbours > 3))
+                                {
+                                    //connection.SendLogMessage(LogLevel.Info, LoggerName, $"Cell {x},{z} {isAlive}. live neighbours: {liveNeighbours}. Killing...");
+                                    //connection.SendLogMessage(LogLevel.Info, LoggerName, $"Killing...");
+                                    update.isAlive = false;
+                                    connection.SendComponentUpdate(c, update);
+                                }
+                                if (!isAlive && liveNeighbours == 3)
+                                {
+                                    //connection.SendLogMessage(LogLevel.Info, LoggerName, $"Cell {x},{z} {isAlive}. live neighbours: {liveNeighbours}. Spawning...");
+                                    //connection.SendLogMessage(LogLevel.Info, LoggerName, $"Spawning...");
+                                    update.isAlive = true;
+                                    connection.SendComponentUpdate(c, update);
+                                }
+                            }
+                        }
+
+                        // Finished doing work...
+                        stopwatch.Stop();
+                        var waitFor = maxWait.Subtract(stopwatch.Elapsed);
+                        System.Threading.Thread.Sleep(waitFor.Milliseconds > 0 ? waitFor : System.TimeSpan.Zero);
                     }
+
                 }
             }
 
@@ -98,5 +210,6 @@ namespace Demo
                 return future.Get();
             }
         }
+                
     }
 }
